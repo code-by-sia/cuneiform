0115546812dbc18e2355bf0757f407edc3774c8b
Sia <me@siamand.cc>
2024-03-03 18:41:51 +0100

    feat: Welcome Page

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -5,6 +5,7 @@ export default function Dashbaord() {
   return (
     <div className="dashboard-page page">
       <h1>Welcome!</h1>
+      <small>A powerful and extendable text editor/viewer app.</small>
     </div>
   )
 }
